1012b9d068c0b0ef218738eff568f4fb6e7c2602
PioAntonio <pioantonioperugini@gmail.com>
2022-11-22 12:39:08 +0100

    removed tslint

--- src/app/pages/api/services/search.service.ts
+++ src/app/pages/api/services/search.service.ts
@@ -1,30 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpService} from "../../../infrastructure/services/http.service";
 import {Observable} from "rxjs";
 import {SearchCommand, SearchResponseDTO} from "../model/search.model";
 import {environment} from "../../../../environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class SearchService {
 
-  constructor(private _http: HttpService) { }
+  constructor(private _http: HttpService) {
+  }
 
   /**
    *Make a search request on gateway
    * */
-  public search=(command:SearchCommand):Observable<SearchResponseDTO>=>{
+  public search = (command: SearchCommand): Observable<SearchResponseDTO> => {
     let url = `${environment.gateway}/searches`;
-    return this._http.post(url,command);
+    return this._http.post(url, command);
   }
 
   /**
    *request history searches for a given query
    * */
-  public history=(query:String, maxResult:number):Observable<SearchResponseDTO>=>{
+  public history = (query: String, maxResult: number): Observable<SearchResponseDTO> => {
     let url = `${environment.gateway}/searches`;
-    return this._http.get(url,{query:query,maxResult:maxResult});
+    return this._http.get(url, {query: query, maxResult: maxResult});
   }
 
 
